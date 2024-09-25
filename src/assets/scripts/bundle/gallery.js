79c63a0414760ad5a3e9579c3b1fc0842d76b18a
madrilene <hola@lenesaile.com>
2024-09-25 09:26:40 +0200

    update gallery

--- src/assets/scripts/bundle/gallery.js
+++ src/assets/scripts/bundle/gallery.js
@@ -1,16 +1,11 @@
 const buttons = document.querySelectorAll('button[data-index]');
 const modals = document.querySelectorAll('dialog');
-const closeButtons = document.querySelectorAll('dialog button');
 buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
     modals[index].showModal();
   });
 });
-closeButtons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    modals[index].close();
-  });
-});
+
 window.addEventListener('click', event => {
   modals.forEach(modal => {
     if (event.target === modal) {
