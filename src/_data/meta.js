be445e836b17df65c8839b158ae61a1f5ed5441b
madrilene <hola@lenesaile.com>
2024-09-25 16:17:11 +0200

    change theme color and author

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -7,10 +7,10 @@ export const locale = 'it_IT';
 export const lang = 'it';
 export const skipContent = 'Salta al contenuto';
 export const author = {
-  name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
+  name: 'Adriano Urso', // i.e. Lene Saile - page / blog author's name. Must be set.
   avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
-  email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
-  website: 'https://www.lenesaile.com' // i.e. https.://www.lenesaile.com - the personal site of the author
+  email: 'ajaneu@gmail.com', // i.e. hola@lenesaile.com - email of the author
+  website: '' // i.e. https.://www.lenesaile.com - the personal site of the author
 };
 export const creator = {
   name: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
@@ -19,7 +19,7 @@ export const creator = {
   social: 'https://front-end.social/@lene'
 };
 export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
-export const themeColor = '#DD4462'; //  Manifest: defines the default theme color for the application
+export const themeColor = '#11697b'; //  Manifest: defines the default theme color for the application
 export const themeBgColor = '#FBFBFB'; // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
 export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
 export const opengraph_default_alt =
